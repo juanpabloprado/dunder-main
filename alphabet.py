0bb62fd3b0cb34971a035923759d25a486727bf4
Juan Prado <juan@juanpabloprado.com>
2024-06-06 16:29:55 -0500

    Initial commit

---
+++ alphabet.py
@@ -0,0 +1,8 @@
+## Challenge ##
+# Write code to find the word in a 
+# list that would come first alphabetically
+
+## Example: When given the list below
+# word_list = ["hamster", "turtle", "cat", "bird"]
+# The code should find "bird" as the word that 
+# would come first alphabetically
